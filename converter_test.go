8d21f81346c814580fa1b7a3ca8629c95765fa09
Matt Aimonetti <mattaimonetti@gmail.com>
2017-12-10 21:54:42 -0800

    reach 100% code exercising

--- converter_test.go
+++ converter_test.go
@@ -14,7 +14,9 @@ func TestToMIDI(t *testing.T) {
 	}{
 		{name: "no patterns"},
 		{name: "single pattern", patterns: []string{"x...x..."}},
+		{name: "last step is a pulse", patterns: []string{"x...x..x"}},
 		{name: "multiple patterns", patterns: []string{"x...x...", "..x...x."}},
+		{name: "multiple similar patterns", patterns: []string{"x...x...", "x...x..."}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
